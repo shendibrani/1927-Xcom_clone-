a5635dec55257f88c429ca53677cedac29283c63
TemplarWarden <daniel.louis.raynor@gmail.com>
2015-11-16 16:28:58 +0100

    Added Edge Scrolling to camera

---
+++ Assets/Scripts/Control System/CameraViewController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(VerticalEdgePanAxis))]
+[RequireComponent(typeof(HorizontalEdgePanAxis))]
+public class CameraViewController : MonoBehaviour {
+
+    VerticalEdgePanAxis verticalAxis;
+    HorizontalEdgePanAxis horizontalAxis;
+
+	// Use this for initialization
+	void Start () {
+        verticalAxis = GetComponent<VerticalEdgePanAxis>();
+        horizontalAxis = GetComponent<HorizontalEdgePanAxis>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(2))
+        {
+            RotateCamera();
+        }
+        else
+        {
+            SlideCamera();
+        }
+	}
+
+    //move camera according to mouse offest in axis values according to dolly rotation
+    void SlideCamera()
+    {
+        transform.position += transform.right * horizontalAxis.axisValue;
+        transform.position += transform.forward * verticalAxis.axisValue;
+    }
+
+    void RotateCamera()
+    {
+        transform.Rotate(0, horizontalAxis.axisValue, 0);
+    }
+
+    public void RotateCamera(Vector3 pRotation)
+    {
+        transform.Rotate(pRotation);
+    }
+
+    public void SetPosition(Transform pPosition)
+    {
+        transform.position = pPosition.position;
+    }
+
+    public void MoveToPostion(Transform pPosition, float pSpeed)
+    {
+
+    }
+}
---
+++ Assets/Scripts/Control System/MouseRotateAxis.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseRotateAxis : Axis
+{
+
+    [SerializeField]
+    float tolerance = 25;
+    Vector2 previousMousePosition;
+    Vector2 deltaMousePosition
+    {
+        get
+        {
+            return (Vector2)Input.mousePosition - previousMousePosition;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _axisValue = 0;
+        if (focus)
+        {
+            if (Input.GetMouseButtonDown(2))
+            {
+                Debug.Log("Test");
+
+            }
+        }
+        previousMousePosition = Input.mousePosition;
+    }
+}
