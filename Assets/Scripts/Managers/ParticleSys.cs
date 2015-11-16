0931f7d501bbdd259f2fc5ffd710fc1af222185a
Shend Ibrani <shend_ibrani@hotmail.com>
2015-11-16 15:29:13 +0100

    SoundManager and ParticleManager added to Managers folder.

---
+++ Assets/Scripts/Managers/ParticleSys.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class CoupledParticle {
+    public GameObject Particle;
+    public ParticleEffect TheParticleEffect;
+}
+
+public enum ParticleEffect {
+    Blood,
+    Spark,
+    ChickenSpawn,
+    EggEffects
+}
+
+public class ParticleSys : MonoBehaviour {
+    /*
+    Particle system with the parameters: lifetime, position, direction, parent
+    Called from anywhere.
+    Bind particle prefab with name
+    */
+    public static ParticleSys instance = null;
+
+    [SerializeField]
+    private List<CoupledParticle> coupledParticleList;
+
+    [SerializeField]
+
+    void Awake() {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+        
+    
+               
+    public void spawnParticle (ParticleEffect pe, Vector3 pPosition, bool looping, float lifetime, Transform pParent)
+    {
+        GameObject go = (GameObject)Instantiate(coupledParticleList.Find(x => x.TheParticleEffect == pe).Particle, pPosition, Quaternion.identity);
+        go.GetComponent<ParticleSystem>().loop = looping;
+        go.transform.parent = pParent;
+        go.GetComponent<ParticleSystem>().startLifetime = lifetime;    
+    }
+
+    public void spawnParticle(ParticleEffect pe, Vector3 pPosition, bool looping, float lifetime) {
+        GameObject go = (GameObject)Instantiate(coupledParticleList.Find(x => x.TheParticleEffect == pe).Particle, pPosition, Quaternion.identity);
+        go.GetComponent<ParticleSystem>().loop = looping;
+        go.GetComponent<ParticleSystem>().startLifetime = lifetime;
+    }
+
+    public void spawnParticle(ParticleEffect pe, Vector3 pPosition, bool looping) {
+        GameObject go = (GameObject)Instantiate(coupledParticleList.Find(x => x.TheParticleEffect == pe).Particle, pPosition, Quaternion.identity);
+        go.GetComponent<ParticleSystem>().loop = looping;
+
+    }
+
+    public void spawnParticle(ParticleEffect pe, Vector3 pPosition)
+    {
+        GameObject go = (GameObject)Instantiate(coupledParticleList.Find(x => x.TheParticleEffect == pe).Particle, pPosition, Quaternion.identity);
+    }
+
+    public void spawnParticleDestroyable(ParticleEffect pe, Vector3 pPosition, float delay)
+    {
+        GameObject go = (GameObject)Instantiate(coupledParticleList.Find(x => x.TheParticleEffect == pe).Particle, pPosition, Quaternion.identity);
+        Destroy(go, delay);
+    }
+}
---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class CoupledSound
+{
+    public SoundEffects TheSoundEffect;
+    public AudioClip[] PlayClips;
+}
+
+public enum SoundEffects {
+    HIT,
+    WALK
+}
+
+public class SoundManager : MonoBehaviour
+{
+
+    private AudioSource source;
+
+    private AudioSource musicPlayer;
+    public AudioClip music;
+    public AudioClip menuMusic;
+
+    public static SoundManager instance = null;
+
+    [SerializeField]
+    private List<CoupledSound> coupledSoundList;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        source = GetComponent<AudioSource>();
+
+    }
+
+    void Start()
+    {
+    
+        musicPlayer = gameObject.AddComponent<AudioSource>();
+        musicPlayer.clip = music;
+        musicPlayer.loop = true;
+        musicPlayer.playOnAwake = true;
+        musicPlayer.Play();
+        
+    }
+
+    public void PlaySound(SoundEffects se)
+    {
+
+        AudioClip[] clipArray = coupledSoundList.Find(x => x.TheSoundEffect == se).PlayClips;
+        
+        int index = Random.Range(0, clipArray.Length);
+        source.clip = clipArray[index];
+
+        if (!source.isPlaying)
+        {
+            source.Play();
+        }
+
+        else
+        {
+
+            AudioSource newSource = gameObject.AddComponent<AudioSource>();
+            newSource.clip = clipArray[index];
+            newSource.Play();
+
+        }
+
+    }
+
+    public void PlaySound(SoundEffects se, bool PlayOneShot)
+    {
+
+        AudioClip[] clipArray = coupledSoundList.Find(x => x.TheSoundEffect == se).PlayClips;
+        int index = Random.Range(0, clipArray.Length);
+        source.clip = clipArray[index];
+
+        if (PlayOneShot) source.PlayOneShot(source.clip);
+        else source.Play();
+
+
+    }
+
+    public void PlaySound(SoundEffects se, bool PlayOneShot, float pVolumeScale)
+    {
+        AudioClip[] clipArray = coupledSoundList.Find(x => x.TheSoundEffect == se).PlayClips;
+        int index = Random.Range(0, clipArray.Length);
+        source.clip = clipArray[index];
+
+
+        if (PlayOneShot) source.PlayOneShot(source.clip, pVolumeScale);
+        else source.Play();
+
+    }
+
+
+}
