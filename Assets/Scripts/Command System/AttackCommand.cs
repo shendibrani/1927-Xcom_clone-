66620f2c6943b3a58591506696d77d43b6704686
TemplarWarden <daniel.louis.raynor@gmail.com>
2015-11-17 16:23:31 +0100

    cleaned attack

--- Assets/Scripts/Command System/AttackCommand.cs
+++ Assets/Scripts/Command System/AttackCommand.cs
@@ -23,20 +23,11 @@ public class AttackCommand : Command {
         {
             Debug.Log(owner + " missed " + target);
         }
-		//if (path == null || path.Count == 0) {
-			//also send UI feedback at some point
-		//	return false;
-		//}
-		//owner.GetComponent<GridMovementBehaviour> ().SetPath (path);
-		//also send UI feedback at some point
 		return true;
 	}
 
 	public override bool Undo ()
 	{
-		//owner.GetComponent<GridMovementBehaviour> ().currentNode = originalPosition;
-		//owner.GetComponent<GridMovementBehaviour> ().position = originalPosition.offsetPosition;
-		//also send UI feedback at some point
 		return true;
 	}
 }
