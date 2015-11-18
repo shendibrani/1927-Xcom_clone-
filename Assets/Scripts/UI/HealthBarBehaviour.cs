42532e15da9685d58e81bfa8eb1997404f1ca27d
Giulio Robecchi <giulio.robecchi@gmail.com>
2015-11-18 11:52:33 +0100

    O hai! I just upgradeded your tooltips.

--- Assets/Scripts/UI/HealthBarBehaviour.cs
+++ Assets/Scripts/UI/HealthBarBehaviour.cs
@@ -1,23 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class HealthBarBehaviour : MonoBehaviour {
-	
-	[SerializeField] SpriteRenderer bar;
-	[SerializeField] Health linkedHealthComponent;
+
+    [SerializeField] Image bar;
+    [SerializeField] Health linkedHealthComponent;
 	
 	float ratio { get { return (float)linkedHealthComponent.health/(float)linkedHealthComponent.maxHealth; } }
 
 	void Start()
 	{
-		bar.transform.localScale = new Vector3(ratio, 1, 1);
+		bar.fillAmount = ratio;
 	}
 
 	void Update () 
 	{
 		if(linkedHealthComponent.maxHealth != 0){
-			bar.transform.localScale = new Vector3(ratio, 1, 1);
-			Debug.Log(ratio);
+			bar.fillAmount += (ratio-bar.fillAmount)*0.2f;
+			//Debug.Log(ratio);
 		}
 	}
 }
