58a44d6acc45917884c90601fb15d8828d0ece52
Shend Ibrani <shend_ibrani@hotmail.com>
2015-11-26 15:30:26 +0100

    I commited LoadXML just now, idk why it appeared again, couldve been in two different places. will check it out to clean

--- Assets/Tiled/Scripts/LoadXML.cs
+++ Assets/Tiled/Scripts/LoadXML.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System;
+using System.Diagnostics;
 
 public class LoadXML : ReadXML {
     public int X_MarginDistance = 1;
@@ -8,16 +9,17 @@ public class LoadXML : ReadXML {
     public int Z_MarginDistance = 1;
 
     private Vector3 instanceLocation;
-    public GameObject ParentOfInstantiations;
     public GameObject defaultInstantiation;
-    
+
     const uint FLIPPED_HORIZONTALLY_FLAG = 0x80000000;
     const uint FLIPPED_VERTICALLY_FLAG = 0x40000000;
     const uint FLIPPED_DIAGONALLY_FLAG = 0x20000000;
     const uint FLIP_MASK = ~(FLIPPED_HORIZONTALLY_FLAG | FLIPPED_VERTICALLY_FLAG | FLIPPED_DIAGONALLY_FLAG);
-    
-    public bool generateMesh;
-    // Use this for initialization
+
+    bool flippedHorizontal;
+    bool flippedVertical;
+    bool flippedDiagonal;
+
     public List<GameObject> PrefabLoader;
     private List<GameObject> generatedObjects = new List<GameObject>();
 
@@ -35,9 +37,12 @@ public class LoadXML : ReadXML {
     {
         if (!runOnce)
         {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
             ReadXMLFile();
             generatedObjects.Clear();
             int counter = 1;
+
             for (int k = 0; k < DEPTH; k++)
             {
                 layerParent = new GameObject();
@@ -45,31 +50,25 @@ public class LoadXML : ReadXML {
                 layerParent.name = "ParentOfLayer" + counter;
                 counter++;
 
-                if (generateMesh) layerParent.AddComponent<CombineChildren>();
-                
                 for (int j = 0; j < HEIGHT; j++)
                 {
                     for (int i = 0; i < WIDTH; i++)
                     {
 
                         instanceLocation = new Vector3( i * X_MarginDistance, k * Y_MarginDistance, j * -Z_MarginDistance);
-                        var tile = data[j, i, k];
-                       
-                        bool flippedHorizontal;
-                        bool flippedVertical;
-                        bool flippedDiagonal;
-
+                        uint tile = data[j, i, k];
+                        
                         int tileID = GetTiledID(tile, out flippedHorizontal, out flippedVertical, out flippedDiagonal);
 
                         if (tileID != 0)
                         {
                             Quaternion rotation = Quaternion.identity;
-                            //if (flippedHorizontal) rotation = Quaternion.Euler(0, 180, 0);
+                       
                             if (flippedVertical & !flippedDiagonal) rotation = Quaternion.Euler(0, 90, 0);
                             else if (flippedDiagonal & !flippedVertical) rotation = Quaternion.Euler(0, 180, 0);
                             else if (flippedDiagonal & flippedVertical) rotation = Quaternion.Euler(0, 270, 0);
 
-                            Debug.Log("Tile " + tileID);
+                            UnityEngine.Debug.Log("Tile " + tileID);
                             tempStore = (GameObject)Instantiate(PrefabLoader[tileID], instanceLocation, rotation);
                             generatedObjects.Add(tempStore);
                             tempStore.transform.parent = layerParent.transform;
@@ -80,7 +79,9 @@ public class LoadXML : ReadXML {
                     }
                 }
             }
-
+            UnityEngine.Debug.Log("Amount of generated objects: " + generatedObjects.Count);
+            sw.Stop();
+            UnityEngine.Debug.Log("Elapsed time to generate structure: " + sw.ElapsedMilliseconds + "ms");
         }
         
     }
@@ -97,19 +98,9 @@ public class LoadXML : ReadXML {
         return (int)(Data & FLIP_MASK);
     }
 
-    public void DestroyOthers() {
-        if (generateMesh & UnityEngine.Application.isPlaying)
-        foreach (GameObject child in GameObject.FindGameObjectsWithTag("CustomGenerated")) {
-            if (child.name != "Combined mesh") {
-                DestroyImmediate(child);
-            }
-        }
-    }
-
     public void DestroyAllLoaded()
     {
-        foreach (GameObject go in GameObject.FindGameObjectsWithTag("CustomGenerated"))
-        {
+        foreach (GameObject go in generatedObjects) {
             DestroyImmediate(go);
         }
     }
