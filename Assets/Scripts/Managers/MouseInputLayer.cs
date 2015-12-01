6259fb19c16b60034c231bff1e8a2b8519657e99
Giulio Robecchi <giulio.robecchi@gmail.com>
2015-12-01 14:02:35 +0100

    Forgot script in the prefab.

---
+++ Assets/Scripts/Managers/MouseInputLayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseInputLayer : MonoBehaviour 
+{
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonUp (0)) 
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit = new RaycastHit();
+
+			if(Physics.Raycast(ray,out hit))
+			{
+				if(SelectionManager.instance.isInTargetingMode)
+				{
+					if(hit.collider.GetComponent<Targetable>() != null)
+					{
+						SelectionManager.target = hit.collider.GetComponent<Targetable>();
+					}
+				} 
+				else 
+				{
+					if(hit.collider.GetComponent<Selectable>() != null)
+					{
+						SelectionManager.selected = hit.collider.GetComponent<Selectable>();
+					}
+				}
+			}
+		}
+	}
+}
