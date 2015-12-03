93ef3c08f66da070ad98f008bd026229c6dbd272
Giulio Robecchi <giulio.robecchi@gmail.com>
2015-12-03 17:03:10 +0100

    Added dialogue interface, added camera collision layer to implement camera movement borders

--- Assets/Scripts/Command System/Command.cs
+++ Assets/Scripts/Command System/Command.cs
@@ -25,10 +25,10 @@ public abstract class Command
 	/// Determines whether t is a valid target for this command. 
 	/// Used to prune the owner's targetlist to calculate the valid targets.
 	/// If the command has multiple conditions, start with the lighter ones to speed the process.
-	/// If the conditions include a typecast (such as x is Pawn) cache the result to prevent multiple calls of the cast function.
+	/// Sort operations from lightest to heaviest when determining the condition.
 	/// </summary>
 	/// <returns><c>true</c> if t is a valid target for the command; otherwise, <c>false</c>.</returns>
-	/// <param name="t"> The targetable that is being analised.</param>
+	/// <param name="t"> The targetable of the gameobject that is being analised.</param>
 	public abstract bool IsValidTarget (Targetable t);
 
 	public override string ToString ()
