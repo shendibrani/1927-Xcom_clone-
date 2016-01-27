a1a0898667e2b0868aae7c6be33eeb47a2e1e916
Giulio Robecchi <giulio.robecchi@gmail.com>
2016-01-27 16:53:48 +0100

    AI pawn, part 1

--- Assets/Scripts/AI/AIPawn.cs
+++ Assets/Scripts/AI/AIPawn.cs
@@ -1,19 +1,68 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class AIPawn : MonoBehaviour
+public class AIPawn : Pawn
 {
-
-	// Use this for initialization
-	void Start ()
-	{
-	
+	public List<Pawn> visibleEnemies{
+		get{
+			return sightList.FindAll(x => x.owner != this.owner);
+		}
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+	public override void Turn ()
 	{
+		base.Turn ();
+
+		NodeBehaviour bestCover = GetBestCoverWithinReach();
+
+		if(currentNode != bestCover){
+			Command move = Factory.GetCommand(Commands.Move, this);
+			move.target = bestCover;
+			move.Execute();
+		}
+	}
+
+	NodeBehaviour GetBestCoverWithinReach()
+	{
+		NodeBehaviour previousBest = currentNode;
 	
+		List<Pawn> enemies = visibleEnemies;
+
+		int bestScore = 0;
+
+		foreach (Pawn enemy in enemies){
+			bestScore += Pawn.GetCoverAtNode(previousBest, enemy);
+		}
+		bestScore /= enemies.Count;
+
+		Dictionary<NodeBehaviour, float> scoringTables = new Dictionary<NodeBehaviour, float>();
+		scoringTables.Add(previousBest,bestScore);
+
+		for(int counter = 0; counter <= ActionPoints; counter++){
+			foreach(NodeBehaviour node in Pathfinder.NodesWithinSteps(currentNode, 3)){
+				if(!scoringTables.ContainsKey(node)){
+					float coverScore = 0;
+					foreach (Pawn enemy in enemies){
+						coverScore += Pawn.GetCoverAtNode(node, enemy);
+					}
+					coverScore /= enemies.Count;
+					scoringTables.Add(node, coverScore);
+				}
+			}
+
+			NodeBehaviour currentBest;
+
+			foreach (NodeBehaviour node in scoringTables.Keys){
+				if(scoringTables[currentBest] < scoringTables[node]){
+					currentBest = node;
+				}
+			}
+
+			if(currentBest == previousBest){
+				return currentBest;
+			}
+		}
 	}
 }
 
