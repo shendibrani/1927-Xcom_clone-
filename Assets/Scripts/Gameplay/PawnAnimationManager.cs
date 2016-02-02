86b9a281aed3b9dea46dd8b2f086f012a5a9fec9
Giulio Robecchi <giulio.robecchi@gmail.com>
2016-02-02 13:42:30 +0100

    Added death and damage animations.

--- Assets/Scripts/Gameplay/PawnAnimationManager.cs
+++ Assets/Scripts/Gameplay/PawnAnimationManager.cs
@@ -3,27 +3,39 @@ using System.Collections;
 
 public class PawnAnimationManager : MonoBehaviour
 {
-	bool dirty;
+	bool skipFrame;
 	bool isAnimating;
-	Animator animator;
+	Animator _animator;
+	Animator animator {
+		get {
+			if (_animator == null) {
+				_animator = GetComponentInChildren<Animator> ();
+			}
+			return _animator;
+		}
+	}
+
 	RuntimeAnimatorController animatorController;
 
 	// Use this for initialization
 	void Start ()
 	{
 		animatorController = GetComponentInChildren<RuntimeAnimatorController> ();
+		GetComponent<Health> ().OnDeath.AddListener (SetDead);
+		GetComponent<Health> ().OnDamage += SetDamaged;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (dirty) {
-			dirty = false;
+		if (skipFrame) {
+			skipFrame = false;
 			return;
 		}
 
 		if (isAnimating) {
 			GetComponentInChildren<Animator> ().SetBool ("Shooting", false);
+			GetComponentInChildren<Animator> ().SetBool ("Damaged", false);
 		}
 
 		if (isAnimating && GetComponentInChildren<Animator> ().GetCurrentAnimatorStateInfo(0).IsTag("Idle")) {
@@ -36,7 +48,18 @@ public class PawnAnimationManager : MonoBehaviour
 	{
 		GetComponentInChildren<Animator> ().SetBool ("Shooting", true);
 		isAnimating = true;
-		dirty = true;
+		skipFrame = true;
+	}
+
+	public void SetDead()
+	{
+		GetComponentInChildren<Animator> ().SetBool ("Dead", true);
+	}
+
+	public void SetDamaged()
+	{
+		GetComponentInChildren<Animator> ().SetBool ("Damaged", true);
+		skipFrame = true;
 	}
 }
 
