caf7a1a68639f6eff04d1311a1feeb7f74788883
Giulio Robecchi <giulio.robecchi@gmail.com>
2016-02-02 12:44:05 +0100

    SetShooting up for testing.

--- Assets/Scripts/Gameplay/PawnAnimationManager.cs
+++ Assets/Scripts/Gameplay/PawnAnimationManager.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class PawnAnimationManager : MonoBehaviour
 {
 	bool dirty;
+	bool isAnimating;
 	Animator animator;
+	RuntimeAnimatorController animatorController;
 
 	// Use this for initialization
 	void Start ()
 	{
 		animator = GetComponentInChildren<Animator> ();
+		animatorController = GetComponentInChildren<RuntimeAnimatorController> ();
 	}
 	
 	// Update is called once per frame
@@ -20,12 +23,20 @@ public class PawnAnimationManager : MonoBehaviour
 			return;
 		}
 
-		animator.SetBool ("Shooting", false);
+		if (isAnimating) {
+			animator.SetBool ("Shooting", false);
+		}
+
+		if (isAnimating && animator.GetCurrentAnimatorStateInfo(0).IsTag("Idle")) {
+			TurnManager.instance.SetFree();
+			isAnimating = false;
+		}
 	}
 
 	public void SetShooting()
 	{
 		animator.SetBool ("Shooting", true);
+		isAnimating = true;
 		dirty = true;
 	}
 }
