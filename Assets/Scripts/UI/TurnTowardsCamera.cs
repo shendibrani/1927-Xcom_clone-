28fd6a115ed1170004c27459aef40bd350bef6eb
Giulio Robecchi <giulio.robecchi@gmail.com>
2016-02-03 11:16:34 +0100

    Fixed tooltip behaviour and cover indicator.

---
+++ Assets/Scripts/UI/TurnTowardsCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnTowardsCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (new Vector3 (
+			Camera.main.transform.position.x, 
+			transform.position.y, 
+			Camera.main.transform.position.z));
+	}
+}
