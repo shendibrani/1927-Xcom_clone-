6772043b26fe9312132fd045c2991de8d7e3fd89
Benita Stärke <benitastaerke987@gmail.com>
2016-02-04 21:25:07 +0100

    how am I supposed to know whats in here

---
+++ Assets/Scripts/GodModeCheat.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GodModeCheat : MonoBehaviour {
+
+	public int _Damage;
+
+	// Update is called once per frame
+	public void Cheat (Pawn _pawn) {
+		Health _H = _pawn.GetComponent<Health> ();
+		if (_H != null) {
+			_H.Damage(_pawn,_Damage);
+		}
+	}
+}
