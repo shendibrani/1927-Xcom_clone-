da53579c1d5ada471acaf0af72de2721b98a03ea
Shend Ibrani <shend_ibrani@hotmail.com>
2016-02-04 17:14:10 +0100

    button sounds in menu and UI layer

---
+++ Assets/Scripts/Gameplay/BarrelExplode.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(Health))]
+public class BarrelExplode : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Health>().OnDeath.AddListener(explode);
+	}
+
+    private void explode(Pawn arg0)
+    {
+        Command cmd = Factory.GetCommand(Commands.Grenade, arg0);
+        cmd.freeExec = true;
+        cmd.Execute();
+    }
+
+}
